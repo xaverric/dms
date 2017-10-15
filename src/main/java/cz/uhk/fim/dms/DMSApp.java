a3822cdb1086c39913aebf1a980228bb4d80750b
Daniel Jilek <daniel.jilek@unicorn.com>
2017-10-15 12:07:22 +0200

    Added category entity - not implemented

--- src/main/java/cz/uhk/fim/dms/DMSApp.java
+++ src/main/java/cz/uhk/fim/dms/DMSApp.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DMSApp {
+
+    private DMSApp(){}
+
     public static void main(String[] args) {
         SpringApplication.run(DMSApp.class, args);
     }
