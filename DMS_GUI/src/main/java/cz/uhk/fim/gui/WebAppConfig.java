c8324bdf14006a821378204f3366f91b552163c2
Daniel Jílek <jilek.daniel@gmail.com>
2017-12-04 22:55:00 +0100

    DMS gui

---
+++ DMS_GUI/src/main/java/cz/uhk/fim/gui/WebAppConfig.java
@@ -0,0 +1,23 @@
+package cz.uhk.fim.gui;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login.html");
+        registry.addViewController("/index").setViewName("index.html");
+        registry.addRedirectViewController("/", "index");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
+    }
+}
