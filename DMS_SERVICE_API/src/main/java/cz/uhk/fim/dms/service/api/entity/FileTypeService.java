453eafa93877bcf06755a0933f53427db3630caa
prikryl <prikryl@localhost>
2017-12-30 14:20:08 +0100

    new services and controlers

--- DMS_SERVICE_API/src/main/java/cz/uhk/fim/dms/service/api/entity/FileTypeService.java
+++ DMS_SERVICE_API/src/main/java/cz/uhk/fim/dms/service/api/entity/FileTypeService.java
@@ -1,7 +1,18 @@
 package cz.uhk.fim.dms.service.api.entity;
 
+import cz.uhk.fim.repository.entity.FileType;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileTypeService {
+
+    FileType getFileTypeBySuffix(String type);
+
+    FileType getFileTypeById(Long id);
+
+    List<FileType> getAllFileTypes();
+
+    FileType addFileType(String name, String suffix, String description);
+
 }
