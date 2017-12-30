486da521ffcc140f10041cb2225ef8bca4ea32e2
prikryl <prikryl@localhost>
2017-12-30 14:20:43 +0100

    new services and controlers

--- DMS_REPOSITORY_API/src/main/java/cz/uhk/fim/repository/dao/api/FileTypeDao.java
+++ DMS_REPOSITORY_API/src/main/java/cz/uhk/fim/repository/dao/api/FileTypeDao.java
@@ -1,4 +1,15 @@
 package cz.uhk.fim.repository.dao.api;
 
+import cz.uhk.fim.repository.entity.FileType;
+import java.util.List;
+
 public interface FileTypeDao {
+    
+    FileType getFileTypeBySuffix(String type);
+
+    FileType getFileTypeById(Long id);
+    
+    List<FileType> getAllFileTypes();
+    
+    FileType addFileType(String name, String suffix, String description);
 }
--- DMS_REPOSITORY_API/src/main/java/cz/uhk/fim/repository/dao/api/RoleDao.java
+++ DMS_REPOSITORY_API/src/main/java/cz/uhk/fim/repository/dao/api/RoleDao.java
@@ -1,4 +1,18 @@
 package cz.uhk.fim.repository.dao.api;
 
+import cz.uhk.fim.repository.entity.Role;
+import java.util.List;
+
 public interface RoleDao {
+    
+    Role getRoleById(Long id);
+    Role getRoleByName(String name);
+    
+    List<Role> getAllRoles();
+    
+    Role addRole(String name, String description, Long parentId);
+    
+    Role updateRoleName(Long id, String name);
+    Role updateRoleDescription(Long id, String description);
+    Role updateRoleParentId(Long id, Long parentId);
 }
