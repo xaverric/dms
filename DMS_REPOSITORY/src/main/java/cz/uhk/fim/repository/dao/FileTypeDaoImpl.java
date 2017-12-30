124504662625f782cfb3ea31f53061acaf4c225c
prikryl <prikryl@localhost>
2017-12-30 14:20:33 +0100

    new services and controlers

--- DMS_REPOSITORY/src/main/java/cz/uhk/fim/repository/dao/FileTypeDaoImpl.java
+++ DMS_REPOSITORY/src/main/java/cz/uhk/fim/repository/dao/FileTypeDaoImpl.java
@@ -2,11 +2,42 @@ package cz.uhk.fim.repository.dao;
 
 import cz.uhk.fim.repository.dao.api.AbstractGenericDAO;
 import cz.uhk.fim.repository.dao.api.FileTypeDao;
-import cz.uhk.fim.repository.entity.File;
+import cz.uhk.fim.repository.entity.FileType;
+import java.util.List;
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Repository
-public class FileTypeDaoImpl extends AbstractGenericDAO<File> implements FileTypeDao {
+public class FileTypeDaoImpl extends AbstractGenericDAO<FileType> implements FileTypeDao {
+
+    @Override
+    public FileType addFileType(String name, String suffix, String description) {
+        FileType ft = new FileType();
+        ft.setName(name);
+        ft.setDescription(description);
+        ft.setSuffix(suffix);       
+        getEntityManager().persist(ft);
+        return ft;
+    }
+
+    @Override
+    public List<FileType> getAllFileTypes() {
+        List<FileType>listFileTypes = getEntityManager().createQuery("from FileType", FileType.class).getResultList();
+        return listFileTypes;
+    }
+
+    @Override
+    public FileType getFileTypeById(Long id) {
+        FileType ft = getEntityManager().find(FileType.class, id);
+        return ft;
+    }
+
+    @Override
+    public FileType getFileTypeBySuffix(String suffix) {
+        Query sql = getEntityManager().createQuery("from FileType where suffix = :suffix", FileType.class);
+        sql.setParameter("suffix", suffix);
+        return getSingleResult(sql);
+    }
 }
--- DMS_REPOSITORY/src/main/java/cz/uhk/fim/repository/dao/RoleDaoImpl.java
+++ DMS_REPOSITORY/src/main/java/cz/uhk/fim/repository/dao/RoleDaoImpl.java
@@ -2,11 +2,100 @@ package cz.uhk.fim.repository.dao;
 
 import cz.uhk.fim.repository.dao.api.AbstractGenericDAO;
 import cz.uhk.fim.repository.dao.api.RoleDao;
-import cz.uhk.fim.repository.entity.File;
+import cz.uhk.fim.repository.entity.Role;
+import java.util.List;
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Repository
-public class RoleDaoImpl extends AbstractGenericDAO<File> implements RoleDao {
+public class RoleDaoImpl extends AbstractGenericDAO<Role> implements RoleDao {
+
+    @Override
+    public Role addRole(String name, String description, Long parentId) {
+        if(parentId == 0 || parentId == null){
+            parentId = getMaxId();
+        }
+        Role role = new Role();
+        role.setName(name);
+        role.setDescription(description);
+        role.setParentId(parentId);
+        getEntityManager().persist(role);
+        return role;
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return getEntityManager().createQuery("from Role", Role.class).getResultList();
+    }
+
+    @Override
+    public Role getRoleById(Long id) {
+        return getRole(id);
+    }
+
+    @Override
+    public Role getRoleByName(String name) {
+        return getRole(name);
+    }
+
+    @Override
+    public Role updateRoleDescription(Long id, String description) {
+        return updateRole(id, description, false);
+    }
+
+    @Override
+    public Role updateRoleName(Long id, String name) {
+        return updateRole(id, name, true);
+    }
+
+    @Override
+    public Role updateRoleParentId(Long id, Long parentId) {
+        return updateRole(id, parentId);
+    }
+
+    private Role getRole(Object value) {
+        Query sql = null;
+        if (value instanceof Long) {
+            sql = getEntityManager().createQuery("from role where id=:id", Role.class);
+            sql.setParameter("id", value);
+        } else if (value instanceof String) {
+            sql = getEntityManager().createQuery("from role where name=:name", Role.class);
+            sql.setParameter("name", value);
+        }
+        Role role = getSingleResult(sql);
+        return role;
+    }
+
+    private Role updateRole(Long id, Long parentId) {
+        return updateRole(id, parentId, false);
+    }
+
+    private Role updateRole(Long id, Object value, boolean name) {
+        Role role = getEntityManager().find(Role.class, id);
+        if (role != null) {
+            role = new Role();
+            if (value instanceof Long) {
+                Long tmpValue = (Long) value;
+                role.setParentId(tmpValue);
+            } else if (value instanceof String) {
+                String tmpValue = (String) value;
+                if (name) {
+                    role.setName(tmpValue);
+                } else {
+                    role.setDescription(tmpValue);
+                }
+            }
+            getEntityManager().merge(role);
+        }
+        return role;
+    }
+    
+    private Long getMaxId(){
+        Long retVal = new Long(0);
+        int tmp = getEntityManager().createQuery("from Role").getResultList().size();
+        retVal = Long.valueOf(tmp);
+        return retVal;
+    }
 }
