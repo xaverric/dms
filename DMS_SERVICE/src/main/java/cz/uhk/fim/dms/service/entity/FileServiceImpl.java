a893f868ce330712a487ef66f83399cd345f37a0
MareCZEk55 <33980891+MareCZEk55@users.noreply.github.com>
2018-01-29 18:44:59 +0100

    file upload a validace (#23)
    odchyceni vyjimky pri spatnem prihlaseni

--- DMS_SERVICE/src/main/java/cz/uhk/fim/dms/service/entity/FileServiceImpl.java
+++ DMS_SERVICE/src/main/java/cz/uhk/fim/dms/service/entity/FileServiceImpl.java
@@ -1,8 +1,10 @@
 package cz.uhk.fim.dms.service.entity;
 
 import cz.uhk.fim.dms.service.api.entity.FileService;
+import cz.uhk.fim.repository.dao.api.FileDao;
 import cz.uhk.fim.repository.dto.api.FileDTO;
 import cz.uhk.fim.repository.entity.File;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,33 +12,34 @@ import java.util.List;
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private FileDao fileDao;
+
     @Override
-    public File getFileByID(Long id) {
-        return null;
-    }
+    public File getFileByID(Long id) {return fileDao.getFileByID(id);}
 
     @Override
     public File getFileByNameUsername(String fileName, String username) {
-        return null;
+        return fileDao.getFileByNameUsername(fileName,username);
     }
 
     @Override
     public File getFileByUsername() {
-        return null;
+        return fileDao.getFileByUsername();
     }
 
     @Override
     public File addFile(FileDTO fileDto) {
-        return null;
+        return fileDao.addFile(fileDto);
     }
 
     @Override
     public File deleteFileVersion(FileDTO fileDto, Long version) {
-        return null;
+        return fileDao.deleteFileVersion(fileDto, version);
     }
 
     @Override
     public List<File> findAllFiles() {
-        return null;
+        return fileDao.findAllFiles();
     }
 }
